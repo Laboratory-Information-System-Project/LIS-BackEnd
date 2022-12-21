57792707c8d74600d2f7dcc588a279e180e17457
EastHonor <ehdgns07@gmail.com>
2022-12-22 01:51:08 +0900

    Update WebConfig.java

--- src/main/java/com/douzone/lis_back/config/WebConfig.java
+++ src/main/java/com/douzone/lis_back/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://dpiezv2v8sa5.cloudfront.net","http://localhost:8080","http://35.78.53.64:8080")
+                .allowedOrigins("http://localhost:3000","https://dpiezv2v8sa5.cloudfront.net","http://localhost:8080","http://35.78.53.64:8080", "http://lis-front-deploy-bucket.s3-website.ap-northeast-2.amazonaws.com/")
                 .allowCredentials(true)
                 .allowedMethods("GET,POST,PUT,OPTIONS,DELETE")
                 .allowedHeaders("X-Requested-With, Content-Type, "
