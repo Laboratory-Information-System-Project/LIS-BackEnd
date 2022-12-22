888ebcf2178af8809f9512fa1a21bfcae1bf00d7
suk97 <weed970728@gmail.com>
2022-12-22 18:16:35 +0900

    i

--- src/main/java/com/douzone/lis_back/config/CORSFilter.java
+++ src/main/java/com/douzone/lis_back/config/CORSFilter.java
@@ -1,45 +1,44 @@
-// package com.douzone.lis_back.config;
-//
-// import java.io.IOException;
-//
-// import javax.servlet.Filter;
-// import javax.servlet.FilterChain;
-// import javax.servlet.FilterConfig;
-// import javax.servlet.ServletException;
-// import javax.servlet.ServletRequest;
-// import javax.servlet.ServletResponse;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
-//
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.stereotype.Component;
-//
-// @Component
-// public class CORSFilter implements Filter{
-//
-//     @Override
-//     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-//             throws IOException, ServletException {
-//         HttpServletResponse response = (HttpServletResponse) res;
-//         HttpServletRequest request= (HttpServletRequest) req;
-//         String host = "https://" + request.getServerName() + ":" +Integer.toString(request.getServerPort());
-//         response.setHeader("Access-Control-Allow-Origin", "http://dpiezv2v8sa5.cloudfront.net/");
-//         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//         response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,OPTIONS,DELETE");
-//         response.setHeader("Access-Control-Allow-Credentials", "true");
-//         response.setHeader("Access-Control-Allow-Max-Age", "3600");
-//         response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, "
-//                 + "Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers" +
-//                 ", Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials" +
-//                 ",Access-Control-Allow-Headers");
-//
-//         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-//             response.setStatus(HttpServletResponse.SC_OK);
-//         } else {
-//             chain.doFilter(req, res);
-//         }
-//     }
-//
-//     public void init(FilterConfig filterConfig) {}
-//     public void destory() {}
-// }
+ package com.douzone.lis_back.config;
+
+ import java.io.IOException;
+
+ import javax.servlet.Filter;
+ import javax.servlet.FilterChain;
+ import javax.servlet.FilterConfig;
+ import javax.servlet.ServletException;
+ import javax.servlet.ServletRequest;
+ import javax.servlet.ServletResponse;
+ import javax.servlet.http.HttpServletRequest;
+ import javax.servlet.http.HttpServletResponse;
+
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.stereotype.Component;
+
+ @Component
+ public class CORSFilter implements Filter{
+
+     @Override
+     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+             throws IOException, ServletException {
+         HttpServletResponse response = (HttpServletResponse) res;
+         HttpServletRequest request= (HttpServletRequest) req;
+         String host = "https://" + request.getServerName() + ":" +Integer.toString(request.getServerPort());
+         response.setHeader("Access-Control-Allow-Origin", "*");
+         response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,OPTIONS,DELETE");
+         response.setHeader("Access-Control-Allow-Credentials", "true");
+         response.setHeader("Access-Control-Allow-Max-Age", "3600");
+         response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, "
+                 + "Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers" +
+                 ", Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials" +
+                 ",Access-Control-Allow-Headers");
+
+         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+             response.setStatus(HttpServletResponse.SC_OK);
+         } else {
+             chain.doFilter(req, res);
+         }
+     }
+
+     public void init(FilterConfig filterConfig) {}
+     public void destory() {}
+ }
