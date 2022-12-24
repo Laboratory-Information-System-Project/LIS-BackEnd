b9fb82c84afed4def1ed1af03e55d12e76083a1c
liverbird <ehdgns07@gmail.com>
2022-12-24 14:45:53 +0900

    login

--- src/main/java/com/douzone/lis_back/config/CORSFilter.java
+++ src/main/java/com/douzone/lis_back/config/CORSFilter.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CORSFilter implements Filter{
-
+//
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
